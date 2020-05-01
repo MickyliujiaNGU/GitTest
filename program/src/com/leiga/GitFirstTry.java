aa4ee91c988abdef0363ed1b99a5704bbb18efc8
e <liujia@qq.com>
2020-05-01 12:57:21 +0800

    programmer1:master:second commit

--- program/src/com/leiga/GitFirstTry.java
+++ program/src/com/leiga/GitFirstTry.java
@@ -2,6 +2,7 @@ package com.leiga;
 
 public class GitFirstTry {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello  world");
+        System.out.println("hello world change");
     }
 }
