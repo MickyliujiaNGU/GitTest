6363d89105cc288a868bd52df4b84c55c02045dd
e <liujia@qq.com>
2020-05-04 19:29:06 +0800

    Revert "AAAmaster changelist"
    This reverts commit 592859a0
    
    Signed-off-by: e <liujia@qq.com>

--- program/src/com/leiga/GitProgrammer1Try.java
+++
@@ -1,9 +0,0 @@
-package com.leiga;
-
-public class GitProgrammer1Try {
-    public static void main(String[] args) {
-        System.out.println("hello  first  programmer 1  changelist test");
-        System.out.println("hello world");
-        System.out.println("hello world  ");
-    }
-}
