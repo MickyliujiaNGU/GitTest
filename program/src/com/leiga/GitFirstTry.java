87095de7414d4c549f29ac579cd791b86055c26e
e <liujia@qq.com>
2020-05-04 15:16:29 +0800

    programmer1 master:look for change

--- program/src/com/leiga/GitFirstTry.java
+++ program/src/com/leiga/GitFirstTry.java
@@ -9,5 +9,6 @@ public class GitFirstTry {
         System.out.println("programmer 2 user commit 2");
         System.out.println("programmer 1 commit second");
         System.out.println("programmer 1 commit third");
+        System.out.println(" look for change");
     }
 }
