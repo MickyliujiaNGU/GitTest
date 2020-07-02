990d9edd7f2491d29ef0102da0d70a67f7499f50
e <liujia@qq.com>
2020-07-02 12:35:54 +0800

    Revert "programmer2：commit"
    This reverts commit 556108ea

--- program/src/com/leiga/ClassLoaderTestImpl.java
+++
@@ -1,9 +0,0 @@
-package com.leiga;
-
-public class ClassLoaderTestImpl extends ClassLoaderTest {
-    static {
-        System.out.println("subClass is loader");
-    }
-
-    public static String sub_String = "sfs";
-}
--- program/src/com/leiga/GitSecondTry.java
+++
@@ -1,4 +0,0 @@
-package com.leiga;
-
-public class GitSecondTry {
-}
