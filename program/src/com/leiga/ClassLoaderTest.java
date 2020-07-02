556108ea3be664af5d913950a7aa0109dd0f0ad9
e <liujia@qq.com>
2020-07-02 11:07:12 +0800

    programmer2：commit

---
+++ program/src/com/leiga/ClassLoaderTest.java
@@ -0,0 +1,12 @@
+package com.leiga;
+
+public class ClassLoaderTest {
+    static {
+        System.out.println("Test class Loader");
+    }
+
+    public static String testClassLoder = "12";
+    public static void method(){
+
+    }
+}
--- program/src/com/leiga/GitFirstTry.java
+++ program/src/com/leiga/GitFirstTry.java
@@ -8,11 +8,12 @@ public class GitFirstTry {
         System.out.println("programmer 2 user");
         System.out.println("programmer 2 user commit 2");
         System.out.println("programmer 1 commit second");
-        System.out.println("programmer 2 master commit 3");
+        System.out.println("programmer 2 mast;er commit 3");
         System.out.println("programmer 1 commit third");
         System.out.println(" look for change");
         System.out.println("programme2 hello fetch");
         System.out.println("programmer2 hello fetch 2");
-        System.out.println("programmer2 hello pull 1");
+        String s;
+
     }
 }
