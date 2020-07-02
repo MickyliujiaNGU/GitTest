02428648ea97d6d72577fbc3d53ffd0aa2e74761
e <liujia@qq.com>
2020-07-02 17:56:58 +0800

    master_rebase_first
    Signed-off-by: e <liujia@qq.com>

--- program/src/com/leiga/TestStringFormat.java
+++ program/src/com/leiga/TestStringFormat.java
@@ -4,5 +4,6 @@ public class TestStringFormat {
     public static void
     main(String[] args) {
         System.out.println(String.format("预期短信[%s]","15"));
+        System.out.println("master rebase commit");
     }
 }
